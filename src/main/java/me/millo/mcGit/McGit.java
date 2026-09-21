42e352847641595062972cfdc4c57873d37ffaea
Millo <23leeuw@gmail.com>
2026-09-21 15:16:01 +0200

    initial commit

---
+++ src/main/java/me/millo/mcGit/McGit.java
@@ -0,0 +1,16 @@
+package me.millo.mcGit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class McGit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("McGit has been enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
